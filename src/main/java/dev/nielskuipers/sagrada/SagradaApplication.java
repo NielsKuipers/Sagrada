d1c858e0583d3fd11ba4ac06806e8e18f83a2ad4
Niels <nielskuipers@duck.com>
2023-01-14 16:55:10 +0100

    init commit

---
+++ src/main/java/dev/nielskuipers/sagrada/SagradaApplication.java
@@ -0,0 +1,13 @@
+package dev.nielskuipers.sagrada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagradaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SagradaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/nielskuipers/sagrada/SagradaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nielskuipers.sagrada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagradaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
