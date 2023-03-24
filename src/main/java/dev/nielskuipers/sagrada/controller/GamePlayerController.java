dd74baf16e3a873548c2c282ffc269e1f7033868
Niels Kuipers <nielskuipers@duck.com>
2023-03-24 01:23:05 +0000

    GamePlayerController service refactor

--- src/main/java/dev/nielskuipers/sagrada/controller/GamePlayerController.java
+++ src/main/java/dev/nielskuipers/sagrada/controller/GamePlayerController.java
@@ -1,74 +1,36 @@
 package dev.nielskuipers.sagrada.controller;
 
-import dev.nielskuipers.sagrada.assembler.GamePlayerModelAssembler;
-import dev.nielskuipers.sagrada.exception.GameExceptions.GameNotFoundException;
-import dev.nielskuipers.sagrada.exception.CardExceptions.ObjectiveCardNotFoundException;
-import dev.nielskuipers.sagrada.exception.PlayerExceptions.PlayerNotFoundException;
-import dev.nielskuipers.sagrada.model.game.*;
-import dev.nielskuipers.sagrada.repository.GamePlayerRepository;
-import dev.nielskuipers.sagrada.repository.GameRepository;
-import dev.nielskuipers.sagrada.repository.ObjectiveCardRepository;
-import dev.nielskuipers.sagrada.repository.PlayerRepository;
+import com.github.fge.jsonpatch.JsonPatch;
+import dev.nielskuipers.sagrada.model.game.GamePlayer;
 import dev.nielskuipers.sagrada.service.GamePlayerService;
-import io.micrometer.common.util.StringUtils;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/api/game/{gameId}/player")
 public class GamePlayerController {
-    private final GamePlayerRepository repository;
-    private final GameRepository gameRepository;
-    private final PlayerRepository playerRepository;
-    private final ObjectiveCardRepository objectiveCardRepository;
-    private final GamePlayerModelAssembler assembler;
     private final GamePlayerService service;
 
-    GamePlayerController(GamePlayerRepository repository, GameRepository gameRepository, PlayerRepository playerRepository, ObjectiveCardRepository objectiveCardRepository, GamePlayerModelAssembler assembler, GamePlayerService service) {
-        this.repository = repository;
-        this.gameRepository = gameRepository;
-        this.playerRepository = playerRepository;
-        this.objectiveCardRepository = objectiveCardRepository;
-        this.assembler = assembler;
+    GamePlayerController(GamePlayerService service) {
         this.service = service;
     }
 
     //join game
-    @PostMapping("/")
-    public ResponseEntity<EntityModel<GamePlayer>> addPlayer(@RequestParam int playerId, @PathVariable int gameId) {
-        Game game = gameRepository.findById(gameId).orElseThrow(() -> new GameNotFoundException(gameId));
-        Player player = playerRepository.findById(playerId).orElseThrow(() -> new PlayerNotFoundException(playerId));
-
-        GamePlayer gamePlayer = new GamePlayer(game, player);
-        GamePlayer newGamePlayer = repository.save(gamePlayer);
-
-        return ResponseEntity.created(linkTo(methodOn(GamePlayerController.class).getGamePlayer(gameId, playerId)).toUri()).body(assembler.toModel(newGamePlayer));
+    @PostMapping("/{playerId}")
+    public ResponseEntity<EntityModel<GamePlayer>> addPlayer(@PathVariable int playerId, @PathVariable int gameId) {
+        return service.addPlayer(playerId, gameId);
     }
 
     // update player
     @PatchMapping("/{playerId}")
     @Modifying
     @Transactional
-    public ResponseEntity<EntityModel<GamePlayer>> updatePlayer(@RequestParam(required = false) Integer playerScore, @RequestParam(required = false) String boardPattern, @PathVariable int gameId, @PathVariable int playerId) {
-        GamePlayer toUpdate = repository.findByGameIdAndPlayerId(gameId, playerId).orElseThrow(() -> new PlayerNotFoundException(playerId));
-
-        if (playerScore != null) toUpdate.setPlayerScore(playerScore);
-        if (StringUtils.isNotBlank(boardPattern)) toUpdate.setBoardPattern(boardPattern);
-
-        GamePlayer updatedGamePlayer = repository.save(toUpdate);
-
-        return ResponseEntity.created(linkTo(methodOn(GamePlayerController.class).getGamePlayer(gameId, playerId)).toUri()).body(assembler.toModel(updatedGamePlayer));
+    public ResponseEntity<EntityModel<GamePlayer>> updatePlayer(@PathVariable int playerId, @PathVariable int gameId, @RequestBody JsonPatch patch ) {
+        return service.updatePlayer(playerId, gameId, patch);
     }
 
     //assign objective card to player
@@ -76,25 +38,14 @@ public class GamePlayerController {
     @Modifying
     @Transactional
     public ResponseEntity<EntityModel<GamePlayer>> assignObjectiveCard(@PathVariable int gameId, @PathVariable int playerId, @PathVariable int objectiveCardId) {
-        ObjectiveCard objectiveCard = objectiveCardRepository.findById(objectiveCardId).orElseThrow(() -> new ObjectiveCardNotFoundException(objectiveCardId));
-        GamePlayer toUpdate = repository.findByGameIdAndPlayerId(gameId, playerId).orElseThrow(() -> new PlayerNotFoundException(playerId));
-
-        toUpdate.setObjectiveCard(objectiveCard);
-        GamePlayer updatedPlayer = repository.save(toUpdate);
-        EntityModel<GamePlayer> entityModel = assembler.toModel(updatedPlayer);
-
-        return ResponseEntity
-                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
-                .body(entityModel);
+        return service.assignObjectiveCard(gameId, playerId, objectiveCardId);
     }
 
     //remove player from game
     @DeleteMapping("/{playerId}")
     @Transactional
     public ResponseEntity<?> removePlayer(@PathVariable int gameId, @PathVariable int playerId) {
-        repository.deleteById(new GamePlayerId(gameId, playerId));
-
-        return ResponseEntity.noContent().build();
+        return service.removePlayer(gameId, playerId);
     }
 
     //get a player from a game
@@ -106,9 +57,6 @@ public class GamePlayerController {
     //get all players from a game
     @GetMapping("/")
     public CollectionModel<EntityModel<GamePlayer>> all(@PathVariable int gameId) {
-        List<EntityModel<GamePlayer>> gamePlayers = new ArrayList<>();
-        repository.findAllByGameId(gameId).forEach(gamePlayer -> gamePlayers.add(assembler.toModel(gamePlayer)));
-
-        return CollectionModel.of(gamePlayers, linkTo(methodOn(GamePlayerController.class).all(gameId)).withSelfRel());
+        return service.all(gameId);
     }
 }
--- src/main/java/dev/nielskuipers/sagrada/service/GamePlayerService.java
+++ src/main/java/dev/nielskuipers/sagrada/service/GamePlayerService.java
@@ -1,19 +1,34 @@
 package dev.nielskuipers.sagrada.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.JsonPatchException;
 import dev.nielskuipers.sagrada.assembler.GamePlayerModelAssembler;
+import dev.nielskuipers.sagrada.controller.GamePlayerController;
+import dev.nielskuipers.sagrada.exception.CardExceptions.ObjectiveCardNotFoundException;
 import dev.nielskuipers.sagrada.exception.GameExceptions.GameNotFoundException;
 import dev.nielskuipers.sagrada.exception.PlayerExceptions.PlayerNotFoundException;
-import dev.nielskuipers.sagrada.model.game.Game;
-import dev.nielskuipers.sagrada.model.game.GamePlayer;
-import dev.nielskuipers.sagrada.model.game.Player;
+import dev.nielskuipers.sagrada.model.game.*;
 import dev.nielskuipers.sagrada.repository.GamePlayerRepository;
 import dev.nielskuipers.sagrada.repository.GameRepository;
 import dev.nielskuipers.sagrada.repository.ObjectiveCardRepository;
 import dev.nielskuipers.sagrada.repository.PlayerRepository;
+import io.micrometer.common.util.StringUtils;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -34,6 +49,28 @@ public class GamePlayerService {
         this.assembler = assembler;
     }
 
+    //get a player from a game
+    public EntityModel<GamePlayer> getGamePlayer(int gameId, int playerId) {
+        GamePlayer player = repository.findByGameIdAndPlayerId(gameId, playerId).orElseThrow(() -> new PlayerNotFoundException(playerId));
+
+        return assembler.toModel(player);
+    }
+
+    //get all players from a game
+    public CollectionModel<EntityModel<GamePlayer>> all(int gameId) {
+        List<EntityModel<GamePlayer>> gamePlayers = new ArrayList<>();
+        repository.findAllByGameId(gameId).forEach(gamePlayer -> gamePlayers.add(assembler.toModel(gamePlayer)));
+
+        return CollectionModel.of(gamePlayers, linkTo(methodOn(GamePlayerController.class).all(gameId)).withSelfRel());
+    }
+
+    //remove a player from a game
+    public ResponseEntity<?> removePlayer(int gameId, int playerId) {
+        repository.deleteById(new GamePlayerId(gameId, playerId));
+
+        return ResponseEntity.noContent().build();
+    }
+
     //join game
     public ResponseEntity<EntityModel<GamePlayer>> addPlayer(int playerId, int gameId) {
         Game game = gameRepository.findById(gameId).orElseThrow(() -> new GameNotFoundException(gameId));
@@ -42,13 +79,43 @@ public class GamePlayerService {
         GamePlayer gamePlayer = new GamePlayer(game, player);
         GamePlayer newGamePlayer = repository.save(gamePlayer);
 
-        return ResponseEntity.created(linkTo(methodOn(GamePlayerService.class).getGamePlayer(gameId, playerId)).toUri()).body(assembler.toModel(newGamePlayer));
+        EntityModel<GamePlayer> entityModel = assembler.toModel(newGamePlayer);
+        return ResponseEntity
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(entityModel);
     }
 
-    //get a player from a game
-    public EntityModel<GamePlayer> getGamePlayer(int gameId, int playerId) {
+    //update entities in player
+    public ResponseEntity<EntityModel<GamePlayer>> updatePlayer(int playerId, int gameId, JsonPatch patch) {
         GamePlayer player = repository.findByGameIdAndPlayerId(gameId, playerId).orElseThrow(() -> new PlayerNotFoundException(playerId));
+        GamePlayer updatedPlayer;
 
-        return assembler.toModel(player);
+        //convert gameplayer to json and apply patch, then map it to a gameplayer object
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode patched = patch.apply(objectMapper.convertValue(player, JsonNode.class));
+            updatedPlayer = objectMapper.treeToValue(patched, GamePlayer.class);
+        } catch (JsonPatchException | JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        EntityModel<GamePlayer> entityModel = assembler.toModel(updatedPlayer);
+        return ResponseEntity
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(entityModel);
+    }
+
+    //assign objective card to player
+    public ResponseEntity<EntityModel<GamePlayer>> assignObjectiveCard(int gameId, int playerId, int objectiveCardId) {
+        ObjectiveCard objectiveCard = objectiveCardRepository.findById(objectiveCardId).orElseThrow(() -> new ObjectiveCardNotFoundException(objectiveCardId));
+        GamePlayer toUpdate = repository.findByGameIdAndPlayerId(gameId, playerId).orElseThrow(() -> new PlayerNotFoundException(playerId));
+
+        toUpdate.setObjectiveCard(objectiveCard);
+        GamePlayer updatedPlayer = repository.save(toUpdate);
+
+        EntityModel<GamePlayer> entityModel = assembler.toModel(updatedPlayer);
+        return ResponseEntity
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(entityModel);
     }
 }
